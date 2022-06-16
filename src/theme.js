52db5ee37c2db60bcab95fbca2c64d459446d51d
NegiBaba <piyushnegi319@gmail.com>
2022-06-16 17:40:48 +0530

    Added baseline styles and required libraries.

---
+++ src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+	palette: {
+		primary: {
+			main: "#FF3B3F",
+		},
+		secondary: {
+			main: "#AF9CF3",
+		},
+		error: {
+			main: "#FF3B3F",
+		},
+		success: {
+			light: "#35D299",
+			main: "#44B77B",
+		},
+	},
+});
+
+export default theme;
